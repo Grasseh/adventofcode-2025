4cbda62855b45110bc684f3df361b5200d38a941
Grasseh <steve@grasseh.com>
2025-12-06 03:13:52 -0500

    Day 6: Trash Compactor - Refactor

--- solvers/solver_6.rb
+++ solvers/solver_6.rb
@@ -5,19 +5,19 @@ require 'pry'
 module Solvers
   class Solver6
     def solve_a(input, _opts = {})
-      arr = input.map(&:strip).map(&:split)
-      lines = arr.size
-      columns = arr.first.size
+      array = input.map(&:strip).map(&:split)
+      lines = array.size
+      columns = array.first.size
 
-      operations = arr.pop
+      operations = array.pop
 
       (0..(columns - 1)).each.reduce(0) do |sum, column|
         op = operations[column]
 
         line_result = if op == '+'
-          add_columns(column, arr, lines)
+          add_columns(column, array, lines)
         else
-          multiply_columns(column, arr, lines)
+          multiply_columns(column, array, lines)
         end
 
         sum + line_result
@@ -25,49 +25,61 @@ module Solvers
     end
 
     def solve_b(input, _opts = {})
-      arr = input.map do |line|
+      array = input.map do |line|
         chars = line.chars
         chars.pop
         chars.join
       end
 
-      lines = arr.size
-      columns = arr.first.size
+      lines_count = array.size
+      columns_count = array.first.size
 
-      i = 0
+      column_index = 0
       sum = 0
-      op = ''
+      operation = ''
       numbers = []
 
-      while i <= columns
-        if i == columns || arr.all? { |x| x[i] == ' ' }
-          line_result = if op == '+'
-            numbers.sum
-          else
-            numbers.reduce(:*)
-          end
-
-          sum += line_result
-          op = ''
+      while column_index <= columns_count
+        if (
+          column_index == columns_count ||
+          array.all? { |x| x[column_index] == ' ' }
+        )
+          sum += calculate_line(operation, numbers)
+          operation = ''
           numbers = []
-          i += 1
+          column_index += 1
           next
         end
 
-        number = (0..(lines - 2)).reduce('') do |str, line_index|
-          (str + arr[line_index][i]).gsub(' ', '')
-        end
+        numbers << get_vertical_number(lines_count, column_index, array)
+        operation = get_operation(array, lines_count, column_index, operation)
 
-        numbers << number.to_i
-
-        if arr[lines - 1][i] != ' '
-          op = arr[lines - 1][i]
-        end
-
-        i += 1
+        column_index += 1
       end
 
       sum
+    end
+
+    def get_operation(array, lines_count, column_index, default)
+      if array[lines_count - 1][column_index] == ' '
+        default
+      else
+        array[lines_count - 1][column_index]
+      end
+    end
+
+    def get_vertical_number(lines_count, column_index, array)
+      (0..(lines_count - 2)).reduce('') do |str, line_index|
+        (str + array[line_index][column_index]).gsub(' ', '')
+      end.to_i
+    end
+
+    def calculate_line(operation, numbers)
+      if operation == '+'
+        numbers.sum
+      else
+        numbers.reduce(:*)
+      end
     end
 
     def add_columns(column_index, array, line_count)
